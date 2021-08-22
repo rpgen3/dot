beb2f132fde3f07b24e909b681b36d21eaaf37c8
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-08-22 11:43:17 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,82 +2,57 @@
     const {importAll, getScript} = await import('https://rpgen3.github.io/mylib/export/import.mjs');
     await getScript('https://rpgen3.github.io/lib/lib/jquery-3.5.1.min.js');
     const rpgen3 = await importAll([
-        'input',
-        'imgur',
-        'strToImg'
+        'input'
     ].map(v => `https://rpgen3.github.io/mylib/export/${v}.mjs`));
-    const undef = void 0;
     const html = $('body').empty().css({
         'text-align': 'center',
         padding: '1em',
-        'user-select': 'none'
+        //'user-select': 'none'
     });
     const header = $('<div>').appendTo(html),
           body = $('<div>').appendTo(html),
           footer = $('<div>').appendTo(html);
-    class SimpleText {
-        constructor({text = '', color = 'black', size = 16}){
-            this.x = this.y = 0;
-            this.text = text;
-            this.color = color;
-            this.size = size;
-            layer.set(this, 999);
+    const cvHolder = $('<div>').appendTo(body).css({
+        position: 'relative',
+        overflow: 'scroll',
+        width: $(window).width() * 0.7,
+        height: $(window).height() * 0.7
+    });
+    const inputZoom = rpgen3.addInputNum(header, {
+        label: 'zoom rate[%]',
+        value: 1,
+        min: 1,
+        max: 9
+    });
+    inputZoom.elm.on('change', () => {
+        $('canvas').css('width', inputZoom * 400);
+    });
+    class Canvas {
+        constructor(parentNode){
+            const zoom = inputZoom * 400;
+            const ctx = $('<canvas>').appendTo(parentNode).prop({
+                width: 400, height: 200
+            }).css({
+                position: 'absolute',
+                //display: 'block',
+                left: 0, top: 0, width: zoom
+            }).get(0).getContext('2d');
+            // ドットを滑らかにしないおまじない
+            ctx.mozImageSmoothingEnabled = false;
+            ctx.webkitImageSmoothingEnabled = false;
+            ctx.msImageSmoothingEnabled = false;
+            ctx.imageSmoothingEnabled = false;
+            this.ctx = ctx;
+            $(ctx.canvas).on('mousedown mousemove touchstart touchmove', e => this.draw(e))
+            //ctx.fillRect(0, 0, 500, 500);
         }
-        update(ctx){
-            const {x, y, text, color, size} = this;
-            ctx.fillStyle = color;
-            ctx.font = `bold ${size}px 'ＭＳ ゴシック'`;
-            ctx.textAlign = 'left';
-            ctx.textBaseline = 'top';
-            ctx.fillText(text, x, y);
-        }
-        goto(x, y){
-            this.x = x;
-            this.y = y;
-            return this;
+        draw(e){
+            if(!e.which) return;
+            const {offsetX, offsetY} = e.originalEvent;
+            console.log(e);
+            const unit = 1, erase = 0;
+            this.ctx[erase ? 'clearRect' : 'fillRect'](offsetX / inputZoom | 0, offsetY / inputZoom | 0, unit, unit);
         }
     }
-    const rpgen4 = await importAll([
-        'Canvas'
-    ].map(v => `https://rpgen3.github.io/game/export/${v}.mjs`));
-    const unit = rpgen3.addInputNum(header,{
-        label: '単位ピクセルの大きさ',
-        save: true,
-        value: 20,
-        min: 30,
-        max: 80
-    });
-    const cvHolder = $('<div>').appendTo(footer).css({position: 'relative'});
-    const makeLayer = () => {
-        const cv = new rpgen4.Canvas(cvHolder);
-        $(cv.ctx.canvas).css({
-            position: 'absolute',
-            left: 0,
-            top: 0
-        });
-        return cv;
-    };
-    const scale = makeLayer();
-    const drawScale = () => {
-        const {ctx, w, h} = scale;
-        ctx.clearRect(0, 0, w, h);
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-        for(let i = 0; i < w; i += unit) ctx.fillRect(i, 0, 1, h);
-        for(let i = 0; i < h; i += unit) ctx.fillRect(0, i, w, 1);
-    };
-    const mainCv = makeLayer();
-    $(cvHolder).on('mousedown mousemove touchstart touchmove', e => draw(e)).on('contextmenu', () => false);
-    let activCv = mainCv;
-    const draw = e => {
-        if(!e.which) return;
-        const {ctx} = activCv;
-        ctx.fillStyle = 'rgba(0, 255, 0, 0.5)';
-        const {left, top} = $(ctx.canvas).offset(),
-              x = quantize(e.clientX - left),
-              y = quantize(e.clientY - top),
-              erase = e.buttons === 2;
-        ctx[erase ? 'clearRect' : 'fillRect'](x, y, unit, unit);
-    };
-    const quantize = v => (v / unit | 0) * unit;
-    unit.elm.on('input', drawScale).trigger('input');
+    for(const i of new Array(5)) new Canvas(cvHolder);
 })();
